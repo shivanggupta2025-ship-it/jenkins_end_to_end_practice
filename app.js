08ee0b73fdd516e49e57be89d9f997e90f718240
shivanggupta2025-ship-it <shivang.gupta2025@vitstudent.ac.in>
2026-04-14 13:34:09 +0530

    8

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ console.log("Version 5 deployed 🚀");
 console.log("FINAL TRIGGER 🔥🔥🔥");
 console.log("Version 6 deployed 🚀");
 console.log("Version 7 deployed 🚀");
+console.log("Version 8 deployed 🚀");
